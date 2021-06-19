9d32bc7da8a7763c590e09425f20562df12662af
leeminq1 <min2195@naver.com>
2021-06-19 15:12:26 +0900

    detail_page

--- components/DayComponent.jsx
+++ components/DayComponent.jsx
@@ -5,48 +5,72 @@ import {
   TouchableOpacity,
   ScrollView,
   Thumbnail,
+  Alert,
 } from "react-native";
 import { Col, Row, Grid } from "react-native-easy-grid";
-import { Icon, Text, Card, CardItem, Button } from "native-base";
+import { Icon, Text, Card, Button,
+  CardItem,
+  Left,
+  Body,
+  Content, } from "native-base";
 import CollapsibleView from "@eliav2/react-native-collapsible-view";
+import { Ionicons } from '@expo/vector-icons'; 
 
 const DayComponent = ({ travel_data, index }) => {
 
+  const [is_view,set_isview]=useState(false);
 
+  // const view_handler=(index)=>{
+  //   return(
+
+  //   )
+  // }
+
+  console.log(index)
 
   return (
- 
-    <ScrollView>
-    <CollapsibleView title={ <Grid>
-        <Grid style={{justifyContent:"space-between",flexDirection:"row"}}>
+    <Content contentContainerStyle={{borderWidth:1,borderColor:"#3498db",borderRadius:13,marginBottom:20,paddingTop:10,paddingBottom:20}}>
+      <Grid>
+        <Grid style={{justifyContent:"space-between",flexDirection:"row",height:50,alignItems:"center"}}>
           <Col size={3}>
-            <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+            <Text style={{ fontSize: 20, fontWeight: "bold",color:"black" }} onPress={()=>{set_isview(prev=>!prev)}}>
               Day {index + 1}
             </Text>
           </Col>
           <Col size={2}>
-            <Text>{travel_data.day}</Text>
+            <Text style={{ fontSize: 20,color:"black" }}>{travel_data.day}</Text>
           </Col>
         </Grid>
-      </Grid>}>
-      </CollapsibleView>
-      <Grid style={{height:300}}>
-        <ScrollView horizontal={true}>
-          {travel_data.image.map((image,index)=>{
-            return(   <Image style={{width:100,height:100}} source={{uri:image}}></Image>)
-         
-
-
-
-          })}
-
-
-        </ScrollView>
-
       </Grid>
-
-      </ScrollView>
-
+      {is_view &&
+      <Grid style={{flexDirection:"column"}}>
+        <ScrollView horizontal={true} style={{marginBottom:20}}>
+        <Grid style={{width:100,height:100,alignItems:"center",justifyContent:"center"}}>
+          <Ionicons name="add-circle-outline" size={35} color="black" onPress={()=>{Alert.alert(index+"번째 이미지업로드")}}/>
+        </Grid>
+            {travel_data.image.map((image,index)=>{
+              return(   <Image style={{width:100,height:100}} source={{uri:image}} key={index}></Image>)
+            })}
+          </ScrollView>
+          {travel_data.journey.map((journey,index)=>{
+                  return(
+                    <Card key={index} >
+                      <CardItem style={{backgroundColor:"#7efff5"}}>
+                        <Left>
+                          <Text style={{fontSize:30,fontWeight:"500",color:"white"}}>{index+1}</Text>
+                          <Body style={{flexDirection:"row",justifyContent:"space-evenly"}}>
+                            <Text style={{fontSize:15,fontWeight:"200",color:"white"}}>{journey.title}</Text>
+                            <Text style={{fontSize:15,fontWeight:"100",color:"white"}}>{journey.role}</Text>
+                            <Text note style={{fontSize:15,fontWeight:"100",color:"white"}}>{journey.address}</Text>
+                          </Body>
+                        </Left>
+                      </CardItem>
+                    </Card>
+                 )
+                })}
+        </Grid>
+        }
+       </Content>
   );
 };
 
--- mapData.js
+++ mapData.js
@@ -108,99 +108,501 @@ export const markers = [
 
 export const travel_data = [
   {
-    coordinate: {
-      latitude: 37.482653409730645,
-      longitude: 126.90354215579079,
-      latitudeDelta: 0.03,
-      longitudeDelta: 0.015090824406148114,
-    },
-    title: "우리집",
-    role: "숙소",
-    address: "서울시 관악구",
     day: "2021-06-01",
     image: [
       "https://static.hubzum.zumst.com/hubzum/2017/07/14/13/90f89393e4db4f5d8c9dda97ef5ffd2b.jpg",
       "https://static.hubzum.zumst.com/hubzum/2017/07/14/13/90f89393e4db4f5d8c9dda97ef5ffd2b.jpg",
+      "https://static.hubzum.zumst.com/hubzum/2017/07/14/13/90f89393e4db4f5d8c9dda97ef5ffd2b.jpg",
+      "https://static.hubzum.zumst.com/hubzum/2017/07/14/13/90f89393e4db4f5d8c9dda97ef5ffd2b.jpg",
+      "https://static.hubzum.zumst.com/hubzum/2017/07/14/13/90f89393e4db4f5d8c9dda97ef5ffd2b.jpg",
+      "https://static.hubzum.zumst.com/hubzum/2017/07/14/13/90f89393e4db4f5d8c9dda97ef5ffd2b.jpg",
+      "https://static.hubzum.zumst.com/hubzum/2017/07/14/13/90f89393e4db4f5d8c9dda97ef5ffd2b.jpg",
+      "https://static.hubzum.zumst.com/hubzum/2017/07/14/13/90f89393e4db4f5d8c9dda97ef5ffd2b.jpg",
+      "https://static.hubzum.zumst.com/hubzum/2017/07/14/13/90f89393e4db4f5d8c9dda97ef5ffd2b.jpg",
+      "https://static.hubzum.zumst.com/hubzum/2017/07/14/13/90f89393e4db4f5d8c9dda97ef5ffd2b.jpg",
+      "https://static.hubzum.zumst.com/hubzum/2017/07/14/13/90f89393e4db4f5d8c9dda97ef5ffd2b.jpg",
+      "https://static.hubzum.zumst.com/hubzum/2017/07/14/13/90f89393e4db4f5d8c9dda97ef5ffd2b.jpg",
+      "https://static.hubzum.zumst.com/hubzum/2017/07/14/13/90f89393e4db4f5d8c9dda97ef5ffd2b.jpg",
     ],
+    journey:[
+      {
+        coordinate: {
+          latitude: 37.482653409730645,
+          longitude: 126.90354215579079,
+          latitudeDelta: 0.03,
+          longitudeDelta: 0.015090824406148114,
+        },
+        title: "우리집",
+        description: "사실은 민규&민지&케로 행복한 민지네집 (민규는 동거인)",
+        role: "숙소",
+        address: "서울시 관악구",
+      },
+      {
+        coordinate: {
+          latitude: 37.47682803986239,
+          longitude: 126.91666177434425,
+          latitudeDelta: 0.03,
+          longitudeDelta: 0.015090824406148114,
+        },
+        title: "내동생민지네",
+        description: "빨래? 설거지? 청소는 언제? 민지네집",
+        role: "숙소",
+        address: "서울시 관악구",
+      },
+      {
+      coordinate: {
+        latitude: 37.468192049433796,
+        longitude: 126.90650219100772,
+        latitudeDelta: 0.03,
+        longitudeDelta: 0.015090824406148114,
+      },
+        title: "장모님집",
+        description: "우리 여보의 마음의 안식처",
+        role: "숙소",
+        address: "서울시 금천구",
+      },
+      {
+        coordinate: {
+          latitude: 37.45759495929232,
+          longitude: 126.90522362201705,
+          latitudeDelta: 0.03,
+          longitudeDelta: 0.015090824406148114,
+        },
+          title: "도마다리",
+          description: "대한민국 감자탕 탑 1, 민규&민지 살찌게한 도마다리",
+          role: "맛집",
+          address: "시흥동",
+        },
+        {
+          coordinate: {
+            latitude: 37.45402900759973,
+            longitude: 126.90277857706612,
+            latitudeDelta: 0.03,
+            longitudeDelta: 0.015090824406148114,
+          },
+            title: "오복순대국",
+            description: "도마다리 먹고 2차가자! 오복순대국",
+            role: "맛집",
+            address: "시흥동",
+          },
+          {
+            coordinate: {
+              latitude: 37.489241054488836,
+              longitude: 127.0520483154253,
+              latitudeDelta: 0.03,
+              longitudeDelta: 0.015090824406148114,
+            },
+            title: "요보 회사",
+              description: "요보회사! 월요병의 근원!",
+              role: "관광명소",
+              address: "도곡동",
+            },
+
+    ]
   },
   {
-    coordinate: {
-      latitude: 37.47682803986239,
-      longitude: 126.91666177434425,
-      latitudeDelta: 0.03,
-      longitudeDelta: 0.015090824406148114,
-    },
-    title: "내동생민지네",
-    role: "숙소",
-    address: "서울시 관악구",
     day: "2021-06-02",
     image: [
       "https://static.hubzum.zumst.com/hubzum/2017/07/14/13/90f89393e4db4f5d8c9dda97ef5ffd2b.jpg",
       "https://static.hubzum.zumst.com/hubzum/2017/07/14/13/90f89393e4db4f5d8c9dda97ef5ffd2b.jpg",
-    ],
+    ], journey:[
+      {
+        coordinate: {
+          latitude: 37.482653409730645,
+          longitude: 126.90354215579079,
+          latitudeDelta: 0.03,
+          longitudeDelta: 0.015090824406148114,
+        },
+        title: "우리집",
+        description: "사실은 민규&민지&케로 행복한 민지네집 (민규는 동거인)",
+        role: "숙소",
+        address: "서울시 관악구",
+      },
+      {
+        coordinate: {
+          latitude: 37.47682803986239,
+          longitude: 126.91666177434425,
+          latitudeDelta: 0.03,
+          longitudeDelta: 0.015090824406148114,
+        },
+        title: "내동생민지네",
+        description: "빨래? 설거지? 청소는 언제? 민지네집",
+        role: "숙소",
+        address: "서울시 관악구",
+      },
+      {
+      coordinate: {
+        latitude: 37.468192049433796,
+        longitude: 126.90650219100772,
+        latitudeDelta: 0.03,
+        longitudeDelta: 0.015090824406148114,
+      },
+        title: "장모님집",
+        description: "우리 여보의 마음의 안식처",
+        role: "숙소",
+        address: "서울시 금천구",
+      },
+      {
+        coordinate: {
+          latitude: 37.45759495929232,
+          longitude: 126.90522362201705,
+          latitudeDelta: 0.03,
+          longitudeDelta: 0.015090824406148114,
+        },
+          title: "도마다리",
+          description: "대한민국 감자탕 탑 1, 민규&민지 살찌게한 도마다리",
+          role: "맛집",
+          address: "시흥동",
+        },
+        {
+          coordinate: {
+            latitude: 37.45402900759973,
+            longitude: 126.90277857706612,
+            latitudeDelta: 0.03,
+            longitudeDelta: 0.015090824406148114,
+          },
+            title: "오복순대국",
+            description: "도마다리 먹고 2차가자! 오복순대국",
+            role: "맛집",
+            address: "시흥동",
+          },
+          {
+            coordinate: {
+              latitude: 37.489241054488836,
+              longitude: 127.0520483154253,
+              latitudeDelta: 0.03,
+              longitudeDelta: 0.015090824406148114,
+            },
+            title: "요보 회사",
+              description: "요보회사! 월요병의 근원!",
+              role: "관광명소",
+              address: "도곡동",
+            },
+
+    ]
   },
   {
-    coordinate: {
-      latitude: 37.468192049433796,
-      longitude: 126.90650219100772,
-      latitudeDelta: 0.03,
-      longitudeDelta: 0.015090824406148114,
-    },
-    title: "장모님집",
-    role: "숙소",
-    address: "서울시 금천구",
     day: "2021-06-03",
     image: [
       "https://static.hubzum.zumst.com/hubzum/2017/07/14/13/90f89393e4db4f5d8c9dda97ef5ffd2b.jpg",
       "https://static.hubzum.zumst.com/hubzum/2017/07/14/13/90f89393e4db4f5d8c9dda97ef5ffd2b.jpg",
-    ],
+    ], journey:[
+      {
+        coordinate: {
+          latitude: 37.482653409730645,
+          longitude: 126.90354215579079,
+          latitudeDelta: 0.03,
+          longitudeDelta: 0.015090824406148114,
+        },
+        title: "우리집",
+        description: "사실은 민규&민지&케로 행복한 민지네집 (민규는 동거인)",
+        role: "숙소",
+        address: "서울시 관악구",
+      },
+      {
+        coordinate: {
+          latitude: 37.47682803986239,
+          longitude: 126.91666177434425,
+          latitudeDelta: 0.03,
+          longitudeDelta: 0.015090824406148114,
+        },
+        title: "내동생민지네",
+        description: "빨래? 설거지? 청소는 언제? 민지네집",
+        role: "숙소",
+        address: "서울시 관악구",
+      },
+      {
+      coordinate: {
+        latitude: 37.468192049433796,
+        longitude: 126.90650219100772,
+        latitudeDelta: 0.03,
+        longitudeDelta: 0.015090824406148114,
+      },
+        title: "장모님집",
+        description: "우리 여보의 마음의 안식처",
+        role: "숙소",
+        address: "서울시 금천구",
+      },
+      {
+        coordinate: {
+          latitude: 37.45759495929232,
+          longitude: 126.90522362201705,
+          latitudeDelta: 0.03,
+          longitudeDelta: 0.015090824406148114,
+        },
+          title: "도마다리",
+          description: "대한민국 감자탕 탑 1, 민규&민지 살찌게한 도마다리",
+          role: "맛집",
+          address: "시흥동",
+        },
+        {
+          coordinate: {
+            latitude: 37.45402900759973,
+            longitude: 126.90277857706612,
+            latitudeDelta: 0.03,
+            longitudeDelta: 0.015090824406148114,
+          },
+            title: "오복순대국",
+            description: "도마다리 먹고 2차가자! 오복순대국",
+            role: "맛집",
+            address: "시흥동",
+          },
+          {
+            coordinate: {
+              latitude: 37.489241054488836,
+              longitude: 127.0520483154253,
+              latitudeDelta: 0.03,
+              longitudeDelta: 0.015090824406148114,
+            },
+            title: "요보 회사",
+              description: "요보회사! 월요병의 근원!",
+              role: "관광명소",
+              address: "도곡동",
+            },
+
+    ]
   },
   {
-    coordinate: {
-      latitude: 37.45759495929232,
-      longitude: 126.90522362201705,
-      latitudeDelta: 0.03,
-      longitudeDelta: 0.015090824406148114,
-    },
-    title: "도마다리",
-    role: "맛집",
-    address: "시흥동",
     day: "2021-06-04",
     image: [
       "https://static.hubzum.zumst.com/hubzum/2017/07/14/13/90f89393e4db4f5d8c9dda97ef5ffd2b.jpg",
       "https://static.hubzum.zumst.com/hubzum/2017/07/14/13/90f89393e4db4f5d8c9dda97ef5ffd2b.jpg",
-    ],
+    ], journey:[
+      {
+        coordinate: {
+          latitude: 37.482653409730645,
+          longitude: 126.90354215579079,
+          latitudeDelta: 0.03,
+          longitudeDelta: 0.015090824406148114,
+        },
+        title: "우리집",
+        description: "사실은 민규&민지&케로 행복한 민지네집 (민규는 동거인)",
+        role: "숙소",
+        address: "서울시 관악구",
+      },
+      {
+        coordinate: {
+          latitude: 37.47682803986239,
+          longitude: 126.91666177434425,
+          latitudeDelta: 0.03,
+          longitudeDelta: 0.015090824406148114,
+        },
+        title: "내동생민지네",
+        description: "빨래? 설거지? 청소는 언제? 민지네집",
+        role: "숙소",
+        address: "서울시 관악구",
+      },
+      {
+      coordinate: {
+        latitude: 37.468192049433796,
+        longitude: 126.90650219100772,
+        latitudeDelta: 0.03,
+        longitudeDelta: 0.015090824406148114,
+      },
+        title: "장모님집",
+        description: "우리 여보의 마음의 안식처",
+        role: "숙소",
+        address: "서울시 금천구",
+      },
+      {
+        coordinate: {
+          latitude: 37.45759495929232,
+          longitude: 126.90522362201705,
+          latitudeDelta: 0.03,
+          longitudeDelta: 0.015090824406148114,
+        },
+          title: "도마다리",
+          description: "대한민국 감자탕 탑 1, 민규&민지 살찌게한 도마다리",
+          role: "맛집",
+          address: "시흥동",
+        },
+        {
+          coordinate: {
+            latitude: 37.45402900759973,
+            longitude: 126.90277857706612,
+            latitudeDelta: 0.03,
+            longitudeDelta: 0.015090824406148114,
+          },
+            title: "오복순대국",
+            description: "도마다리 먹고 2차가자! 오복순대국",
+            role: "맛집",
+            address: "시흥동",
+          },
+          {
+            coordinate: {
+              latitude: 37.489241054488836,
+              longitude: 127.0520483154253,
+              latitudeDelta: 0.03,
+              longitudeDelta: 0.015090824406148114,
+            },
+            title: "요보 회사",
+              description: "요보회사! 월요병의 근원!",
+              role: "관광명소",
+              address: "도곡동",
+            },
+
+    ]
   },
   {
-    coordinate: {
-      latitude: 37.45402900759973,
-      longitude: 126.90277857706612,
-      latitudeDelta: 0.03,
-      longitudeDelta: 0.015090824406148114,
-    },
-    title: "오복순대국",
-    role: "맛집",
-    address: "시흥동",
     day: "2021-06-05",
     image: [
       "https://static.hubzum.zumst.com/hubzum/2017/07/14/13/90f89393e4db4f5d8c9dda97ef5ffd2b.jpg",
       "https://static.hubzum.zumst.com/hubzum/2017/07/14/13/90f89393e4db4f5d8c9dda97ef5ffd2b.jpg",
-    ],
+    ], journey:[
+      {
+        coordinate: {
+          latitude: 37.482653409730645,
+          longitude: 126.90354215579079,
+          latitudeDelta: 0.03,
+          longitudeDelta: 0.015090824406148114,
+        },
+        title: "우리집",
+        description: "사실은 민규&민지&케로 행복한 민지네집 (민규는 동거인)",
+        role: "숙소",
+        address: "서울시 관악구",
+      },
+      {
+        coordinate: {
+          latitude: 37.47682803986239,
+          longitude: 126.91666177434425,
+          latitudeDelta: 0.03,
+          longitudeDelta: 0.015090824406148114,
+        },
+        title: "내동생민지네",
+        description: "빨래? 설거지? 청소는 언제? 민지네집",
+        role: "숙소",
+        address: "서울시 관악구",
+      },
+      {
+      coordinate: {
+        latitude: 37.468192049433796,
+        longitude: 126.90650219100772,
+        latitudeDelta: 0.03,
+        longitudeDelta: 0.015090824406148114,
+      },
+        title: "장모님집",
+        description: "우리 여보의 마음의 안식처",
+        role: "숙소",
+        address: "서울시 금천구",
+      },
+      {
+        coordinate: {
+          latitude: 37.45759495929232,
+          longitude: 126.90522362201705,
+          latitudeDelta: 0.03,
+          longitudeDelta: 0.015090824406148114,
+        },
+          title: "도마다리",
+          description: "대한민국 감자탕 탑 1, 민규&민지 살찌게한 도마다리",
+          role: "맛집",
+          address: "시흥동",
+        },
+        {
+          coordinate: {
+            latitude: 37.45402900759973,
+            longitude: 126.90277857706612,
+            latitudeDelta: 0.03,
+            longitudeDelta: 0.015090824406148114,
+          },
+            title: "오복순대국",
+            description: "도마다리 먹고 2차가자! 오복순대국",
+            role: "맛집",
+            address: "시흥동",
+          },
+          {
+            coordinate: {
+              latitude: 37.489241054488836,
+              longitude: 127.0520483154253,
+              latitudeDelta: 0.03,
+              longitudeDelta: 0.015090824406148114,
+            },
+            title: "요보 회사",
+              description: "요보회사! 월요병의 근원!",
+              role: "관광명소",
+              address: "도곡동",
+            },
+
+    ]
   },
   {
-    coordinate: {
-      latitude: 37.489241054488836,
-      longitude: 127.0520483154253,
-      latitudeDelta: 0.03,
-      longitudeDelta: 0.015090824406148114,
-    },
-    title: "요보 회사",
-    role: "관광명소",
-    address: "도곡동",
     day: "2021-06-06",
     image: [
       "https://static.hubzum.zumst.com/hubzum/2017/07/14/13/90f89393e4db4f5d8c9dda97ef5ffd2b.jpg",
       "https://static.hubzum.zumst.com/hubzum/2017/07/14/13/90f89393e4db4f5d8c9dda97ef5ffd2b.jpg",
-    ],
+    ], journey:[
+      {
+        coordinate: {
+          latitude: 37.482653409730645,
+          longitude: 126.90354215579079,
+          latitudeDelta: 0.03,
+          longitudeDelta: 0.015090824406148114,
+        },
+        title: "우리집",
+        description: "사실은 민규&민지&케로 행복한 민지네집 (민규는 동거인)",
+        role: "숙소",
+        address: "서울시 관악구",
+      },
+      {
+        coordinate: {
+          latitude: 37.47682803986239,
+          longitude: 126.91666177434425,
+          latitudeDelta: 0.03,
+          longitudeDelta: 0.015090824406148114,
+        },
+        title: "내동생민지네",
+        description: "빨래? 설거지? 청소는 언제? 민지네집",
+        role: "숙소",
+        address: "서울시 관악구",
+      },
+      {
+      coordinate: {
+        latitude: 37.468192049433796,
+        longitude: 126.90650219100772,
+        latitudeDelta: 0.03,
+        longitudeDelta: 0.015090824406148114,
+      },
+        title: "장모님집",
+        description: "우리 여보의 마음의 안식처",
+        role: "숙소",
+        address: "서울시 금천구",
+      },
+      {
+        coordinate: {
+          latitude: 37.45759495929232,
+          longitude: 126.90522362201705,
+          latitudeDelta: 0.03,
+          longitudeDelta: 0.015090824406148114,
+        },
+          title: "도마다리",
+          description: "대한민국 감자탕 탑 1, 민규&민지 살찌게한 도마다리",
+          role: "맛집",
+          address: "시흥동",
+        },
+        {
+          coordinate: {
+            latitude: 37.45402900759973,
+            longitude: 126.90277857706612,
+            latitudeDelta: 0.03,
+            longitudeDelta: 0.015090824406148114,
+          },
+            title: "오복순대국",
+            description: "도마다리 먹고 2차가자! 오복순대국",
+            role: "맛집",
+            address: "시흥동",
+          },
+          {
+            coordinate: {
+              latitude: 37.489241054488836,
+              longitude: 127.0520483154253,
+              latitudeDelta: 0.03,
+              longitudeDelta: 0.015090824406148114,
+            },
+            title: "요보 회사",
+              description: "요보회사! 월요병의 근원!",
+              role: "관광명소",
+              address: "도곡동",
+            },
+
+    ]
   },
 ];
